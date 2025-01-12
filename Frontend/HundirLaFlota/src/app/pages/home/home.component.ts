ed81dd6d6f5f8f4b9bf8bac362e15621a10fd488
dcuecal515 <dcuecal515@g.educaand.es>
2025-01-12 19:25:59 +0100

    Mejora en el movimiento con el barco

--- Frontend/HundirLaFlota/src/app/pages/home/home.component.ts
+++ Frontend/HundirLaFlota/src/app/pages/home/home.component.ts
@@ -9,10 +9,17 @@ import { Component,HostListener, ViewChild,ElementRef } from '@angular/core';
 })
 export class HomeComponent {
   currentOpacity: number=0
+  curretTop: string = "80%"
   @ViewChild('rule1', { static: true }) rule1!: ElementRef;
+  @ViewChild('ship', { static: true }) ship!: ElementRef;
   @HostListener('window:scroll', [])
   checkScroll(){
     const currentScroll = window.scrollY;
+    /*if(currentScroll<400){
+      this.ship.nativeElement.style.top = this.curretTop;
+    }else{
+      this.ship.nativeElement.style.top = "80%";
+    }*/
     if(currentScroll>400 && this.rule1.nativeElement.style.opacity !== ('1')){
       this.currentOpacity+=0.02;
       this.rule1.nativeElement.style.opacity = this.currentOpacity.toString();
