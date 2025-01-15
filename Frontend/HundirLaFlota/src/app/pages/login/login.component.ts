7ea45ca96c8b4102934614c1ed46f8796bf62e94
FranSiles <91953373+FranSiles@users.noreply.github.com>
2025-01-15 20:37:10 +0100

    cambios tochos en el front

--- Frontend/HundirLaFlota/src/app/pages/login/login.component.ts
+++ Frontend/HundirLaFlota/src/app/pages/login/login.component.ts
@@ -1,13 +1,45 @@
 import { Component,ViewChild,ElementRef } from '@angular/core';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AuthserviceService } from '../../services/authservice.service';
+import { Login } from '../../models/Login';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [FormsModule,ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  constructor(private formBuilder: FormBuilder,private authservice:AuthserviceService){
+    this.loginForm = this.formBuilder.group({
+      name: ['', [Validators.required]],
+      password: ['', [Validators.required]]
+    })
+  }
+  /*esto es para las peticiones que hagamos en este componente*/
+  loginForm: FormGroup;
+  
+  name=""
+  password=""
+
+  async loginUser():Promise<void>{
+    if(this.loginForm.valid){
+      const Date:Login={name: this.name.trim(),password: this.password.trim()}
+      console.log(Date)
+      const result = await this.authservice.login(Date);
+      
+    }else{
+      alert("Los datos introducidos son invalidos")
+    }
+    
+  }
+  remenberfunction()
+
+
+
+
+  /*Esto es typescript para la vison de la pagina*/
   @ViewChild('login', { static: true }) login!: ElementRef;
   @ViewChild('signup', { static: false }) signup!: ElementRef;
   
---
+++ Frontend/HundirLaFlota/src/app/services/authservice.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { Login } from '../models/Login';
+import { Token } from '../models/token';
+import { Result } from '../models/result';
+import { ApiService } from './api.service';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthserviceService {
+
+  constructor(private api: ApiService) { }
+  async login(login:Login):Promise<Result<Token>>{
+    const result=await this.api.post<Token>('login',login)
+    if(result.success){
+      return result
+    }
+  }
+}
