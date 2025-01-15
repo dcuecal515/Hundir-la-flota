642da28c17bdd784ced7646c825dc0817d0582d1
dcuecal515 <dcuecal515@g.educaand.es>
2025-01-15 14:02:14 +0100

    Subida de partes importantes en el back

---
+++ Backend/Server/Server/Repositories/UserRepository.cs
@@ -0,0 +1,13 @@
+﻿using Server.Repositories.Base;
+using Server.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Server.Repositories
+{
+    public class UserRepository : Repository<User, int>
+    {
+        public UserRepository(HundirLaFlotaContext context) : base(context){ }
+
+
+    }
+}
---
+++ Backend/Server/Server/UnitOfWork.cs
@@ -0,0 +1,13 @@
+﻿namespace Server
+{
+    public class UnitOfWork
+    {
+        private readonly HundirLaFlotaContext _context;
+        public UnitOfWork(HundirLaFlotaContext context)
+        {
+            _context = context;
+        }
+
+        
+    }
+}
