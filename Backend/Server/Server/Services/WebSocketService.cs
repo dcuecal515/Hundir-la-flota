40a58b589830713083cca4d79505e8caea2dca4e
dcuecal515 <dcuecal515@g.educaand.es>
2025-01-24 14:17:17 +0100

    Subida de funciones por editar

--- Backend/Server/Server/Services/WebSocketService.cs
+++ Backend/Server/Server/Services/WebSocketService.cs
@@ -23,6 +23,15 @@ namespace Server.Services
             while (webSocket.State == WebSocketState.Open)
             {
 
+                string message = await ReadAsync(webSocket);
+
+                if (!string.IsNullOrEmpty(message))
+                {
+                    string outMessage = $"[{string.Join(", ", message as IEnumerable<char>)}]";
+
+                    await SendAsync(webSocket, outMessage);
+                }
+
                 /*UserDateDto userDateDto=_userMapper.toDto(user);
                 string apoyo = JsonSerializer.Serialize(userDateDto);
                 byte[] bytes = Encoding.UTF8.GetBytes(apoyo);
@@ -39,6 +48,33 @@ namespace Server.Services
 
             // Enviamos los bytes al cliente marcando que el mensaje es un texto
             return webSocket.SendAsync(bytes, WebSocketMessageType.Text, true, cancellation);
+        }
+
+        private async Task<string> ReadAsync(WebSocket webSocket, CancellationToken cancellation = default)
+        {
+            byte [] buffer = new byte [4096];
+            StringBuilder stringBuilder = new StringBuilder();
+            bool endOfMessage = false;
+
+            do
+            {
+                WebSocketReceiveResult result = await webSocket.ReceiveAsync(buffer, cancellation);
+
+                if (result.MessageType == WebSocketMessageType.Text)
+                {
+                    string message = Encoding.UTF8.GetString(buffer, 0, result.Count);
+                    stringBuilder.Append(message);
+                }
+                else if (result.CloseStatus.HasValue)
+                {
+                    await webSocket.CloseAsync(result.CloseStatus.Value, result.CloseStatusDescription, cancellation);
+                }
+
+                endOfMessage = result.EndOfMessage;
+            }
+            while (webSocket.State == WebSocketState.Open && !endOfMessage);
+
+            return stringBuilder.ToString();
         }
 
     }
