ebe3d20eca6e6028bdcdd6abf78236271027fc88
FranSiles <francisco.siles.fp@iescampanillas.com>
2025-02-13 13:10:13 +0100

    subida de creacion de un tablero temporal

--- Frontend/HundirLaFlota/src/app/pages/party/party.component.ts
+++ Frontend/HundirLaFlota/src/app/pages/party/party.component.ts
@@ -9,4 +9,17 @@ import { Component } from '@angular/core';
 })
 export class PartyComponent {
 
+  items=[1,2,3,4,5,6,7,8,9,10]
+  letras=['a','b','c','d','e','f','g','h','i','j']
+  barcos=[]
+  barcosoponente=[]
+
+  guardarposicion(letra:string,item:number){
+    var miposicion
+    var gamebox=document.getElementById("game-box")
+    gamebox.classList.remove("game-box")
+    gamebox.classList.add("game-box-view")
+    miposicion=letra+item
+    console.log(miposicion)
+  }
 }
