e4867c59ec0f11fda7b17cbd9e6a840c1a93bb70
FranSiles <francisco.siles.fp@iescampanillas.com>
2025-02-14 13:28:37 +0100

    subida de comienzo de controlacion del drag and drop

--- Frontend/HundirLaFlota/src/app/pages/party/party.component.ts
+++ Frontend/HundirLaFlota/src/app/pages/party/party.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component,AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-party',
@@ -7,29 +7,28 @@ import { Component } from '@angular/core';
   templateUrl: './party.component.html',
   styleUrl: './party.component.css'
 })
-export class PartyComponent {
+export class PartyComponent implements AfterViewInit {
 
   items=[1,2,3,4,5,6,7,8,9,10]
   letras=['a','b','c','d','e','f','g','h','i','j']
   barcos=[]
   barcosoponente=[]
-  barco4Id=document.getElementById("barco4")
-  dropZone = document.querySelector(".game-box");
+  ngAfterViewInit() {
+    const dropzone = document.getElementById('dropzone');
+    const draggable = document.getElementById('barco4');
+    draggable.addEventListener('dragstart', (event) => {
+      event.dataTransfer.setData('text/plain', draggable.id);
+      console.log('Estoy arrastrando mi elemento');
+    });
+  }
   cambiarposicionbarco4(){
     var barco4=document.getElementById("barco4");
     if(barco4.classList.contains("barco4-column")){
       barco4.classList.remove("barco4-column")
       barco4.classList.add("barco4-row")
-      console.log("ADIOS")
     }else{
-      console.log("HOLA")
       barco4.classList.remove("barco4-row")
       barco4.classList.add("barco4-column")
     }
-    /*if(barco4.classList.contains("barco4-row")){
-      barco4.classList.remove("barcos4-row")
-      barco4.classList.add("barco4-column")
-      console.log("HOLA")
-    }*/
   }
 }
